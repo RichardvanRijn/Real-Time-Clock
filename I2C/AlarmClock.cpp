703af70ce6a1f1c5fbbe0c369a24610d4ddc29ad
Richard van Rijn <richardrijn@planet.nl>
2015-02-06 00:18:15 +0100

    moved to interupt
    Waiting is now handeled bij een interupt on timer 0

--- I2C/AlarmClock.cpp
+++ I2C/AlarmClock.cpp
@@ -5,11 +5,23 @@
 #include <avr/pgmspace.h>
 #include <stdbool.h>
 #include <util/delay.h>
-#include <inttypes.h>
 #include "lcd.h"
 #include "I2C.h"
 
 #define address 0xD0
+
+char buffer[17];
+unsigned char time[7];
+unsigned char data[7];
+
+I2C TWI(address);
+
+void init_T0(void)
+{
+	TCCR0 = (1<<CS02) && (1<<CS00);
+	TCNT0 = 0;
+	TIMSK = (1<<TOIE0);
+}
 
 unsigned char bcdToDec(unsigned char val)
 {
@@ -21,30 +33,77 @@ unsigned char decToBcd(unsigned char val)
 	return ((val/10*16) + (val%10) );
 }
 
-char buffer[17];
-unsigned char time[7];
-unsigned char data[7];
-unsigned char timeIn[8] {0x00,	 decToBcd(0), decToBcd(0), decToBcd(0), decToBcd(0),	 decToBcd(0), decToBcd(0), decToBcd(0),};
-//	 Address,Seconds,	  Minutes,	   Hours,	    Day of the week, Date,		  month,	   Year.
+//unsigned char timeIn[8] {0x00,	 decToBcd(0), decToBcd(0), decToBcd(0), decToBcd(0),	 decToBcd(0), decToBcd(0), decToBcd(0)};
+//						   Address	,Seconds,	  Minutes,	   Hours,	    Day of the week, Date,		  month,	   Year.
+
+void writeScreen(void)
+{
+	lcd_home();
+	snprintf(buffer, sizeof buffer,"%d:",time[2]);
+	lcd_puts(buffer);
+	lcd_goto(0,3);
+	snprintf(buffer, sizeof buffer,"%d:",time[1]);
+	lcd_puts(buffer);
+	lcd_goto(0,6);
+	snprintf(buffer, sizeof buffer,"%d ",time[0]);
+	lcd_puts(buffer);
+	lcd_goto(1,0);
+	snprintf(buffer, sizeof buffer,"%d-",time[4]);
+	lcd_puts(buffer);
+	lcd_goto(1,3);
+	snprintf(buffer, sizeof buffer,"%d-",time[5]);
+	lcd_puts(buffer);
+	lcd_goto(1,6);
+	snprintf(buffer, sizeof buffer,"%d ",time[6]);
+	lcd_puts(buffer);	
+}
+
+void changeTime(void)
+{
+	lcd_cursor(true,true);
+	lcd_home();
+	
+}
+
+ISR(TIMER0_OVF_vect) {
+	static unsigned char x = 0;
+
+	if ( x == 15 )
+	{
+		TWI.start_wait(I2C_WRITE);			// set device address and write mode
+		TWI.readXBytes(data,7,0x00);
+		TWI.stop();
+		for (uint8_t i = 0; i <= 7 ; i++)
+		{
+			time[i] = bcdToDec(data[i]);
+		}
+		x = 0;
+	}
+	else
+	{
+		x++;
+	}
+	TCNT0 = 0;
+}
 
 int main(void)
 {
 	DDRD = 0x00;
-	I2C TWI(address);
 	lcd_init();
 	lcd_cursor(false, false);							//  cursor off
 	lcd_home();
-	TWI.init();											// Function to initialize TWI
 	snprintf(buffer, sizeof buffer, "int");
 	lcd_puts(buffer);
 	lcd_home();
+	TWI.init();									// Function to initialize TWI
 	int ret = TWI.start(I2C_WRITE);				// set device address and write mode
-	if ( ret ) {										// failed to issue start condition, possibly no device found
+	if ( ret ) 
+	{										// failed to issue start condition, possibly no device found
 		TWI.stop();
 		snprintf(buffer, sizeof buffer, "Failed");
 		lcd_puts(buffer);
 		lcd_home();
-		}
+	}
 	else 
 	{
 /*		TWI.stop();
@@ -52,35 +111,17 @@ int main(void)
 		TWI.writeXBytes(timeIn,8);
 		TWI.stop();
 */
+		init_T0();
+		sei();
 		while(1)
 		{
-			TWI.start_wait(I2C_WRITE);			// set device address and write mode	
-			TWI.readXBytes(data,7,0x00);
-			TWI.stop();
-			for (uint8_t i = 0; i <= 7 ; i++)
-			{
-				time[i] = bcdToDec(data[i]);
-			}
-			lcd_home();
-			snprintf(buffer, sizeof buffer,"%d:",time[2]);
-			lcd_puts(buffer);
-			lcd_goto(0,3);
-			snprintf(buffer, sizeof buffer,"%d:",time[1]);
-			lcd_puts(buffer);
-			lcd_goto(0,6);
-			snprintf(buffer, sizeof buffer,"%d ",time[0]);
-			lcd_puts(buffer);
-			lcd_goto(1,0);
-			snprintf(buffer, sizeof buffer,"%d-",time[4]);
-			lcd_puts(buffer);
-			lcd_goto(1,3);
-			snprintf(buffer, sizeof buffer,"%d-",time[5]);
-			lcd_puts(buffer);
-			lcd_goto(1,6);
-			snprintf(buffer, sizeof buffer,"%d ",time[6]);
-			lcd_puts(buffer);
-			_delay_ms(1000);
-			TWI.read(0x00);
+			cli();
+			writeScreen();
+			sei();
+//			if (PIND & (1<<0))
+//			{
+//				changeTime();	
+//			}
 		}
 	}
 }
