b8f8a3c14757090addf26ae6dc8582d55a3ace30
Richard van Rijn <richardrijn@planet.nl>
2015-02-12 15:17:40 +0100

    implemented button 1-3

--- I2C/AlarmClock.cpp
+++ I2C/AlarmClock.cpp
@@ -15,6 +15,9 @@ unsigned char time[13];
 unsigned char data[7];
 unsigned char temp;
 
+unsigned char index[2][16] = { 5, 4, 16, 3, 2, 16, 1, 0, 16, 16, 16, 16, 16, 16, 16, 16 }
+							 { 8, 7, 16, 10, 9, 16, 12, 11, 16 ,16, 16, 16, 16, 16, 16, 16};
+
 I2C TWI(address);
 
 void init_T0(void)
@@ -73,11 +76,11 @@ void changeTime(void)
 		}
 		else if (PIND & (1<<2))
 		{
-			
+			time[index[lcd_get_row][lcd_get_column]]++;
 		}
 		else if (PIND & (1<<3))
 		{
-			
+			time[index[lcd_get_row][lcd_get_column]]--;
 		}
 	 }
 }
@@ -146,10 +149,10 @@ int main(void)
 			cli();
 			writeScreen();
 			sei();
-//			if (PIND & (1<<0))
-//			{
-//				changeTime();	
-//			}
+			if (PIND & (1<<0))
+			{
+				changeTime();
+			}
 		}
 	}
 }
